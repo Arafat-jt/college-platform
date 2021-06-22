01de4404b2ee6b46939d8fb1a0f51586ebf58046
arshkhan <khanarsh322@gmail.com>
2021-06-22 23:20:18 +0530

    navBarTheme(Toggle)-OK

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  chTheme = "" ;
+
   constructor() { }
+
+  lighttheme(event){
+    console.log(event.detail.checked);
+    if (event.detail.checked) {
+      document.body.setAttribute('color-theme','dark');
+      this.chTheme = 'white';
+    }
+    else{
+      document.body.setAttribute('color-theme','light');
+      this.chTheme = 'black';
+    }
+  }
 
   ngOnInit() {}
 
